3e193fdb904d4fcae97ca9c0b056b97b20ac38e6
trentboivin <trent.boivin@gmail.com>
2022-05-16 17:11:50 -0400

    update 1.0 - Added TODO comments to main. Created a new java file with a class labeled ToolBox to hold our methods and objects for our main java file.

--- DataSetProject/src/DataProject.java
+++ DataSetProject/src/DataProject.java
@@ -1,12 +1,21 @@
 /**
  * @author Trent
- *
+ * Date Created: 5/16/2022
+ * Project Purpose: To help us learn how to sort and store data as well as accessing it and verifying the data.
+ * 
+ * COLLABARATORS: ERIK BARTZ - TRENT BOIVIN
  */
 public class DataProject {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// TODO Create classes for data storage.
+		// TODO figure out data types and variables/finals 
+		// TODO import scanner library and create scanner
+		// TODO create toolbox with random verificating method for the "robot check" when creating an account
+		// TODO Figure out the roadmap for creating this program. 
+		
+		
+		
 	}
 
 }
---
+++ DataSetProject/src/ToolBox.java
@@ -0,0 +1,11 @@
+/**
+ * Date Created: 5/16/2022
+ * Purpose: Toolbox full of our own methods and objects to be used in our main java program
+ * 
+ * COLLABORATORS: ERIK BARTZ - TRENT BOIVIN
+ */
+public class ToolBox {
+	
+	
+
+}
