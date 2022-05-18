9a6939e78d93742a85aa49c80b1bc3f1d1e8bab2
trentboivin <trent.boivin@gmail.com>
2022-05-18 16:53:42 -0400

    update 1.4
    Deleted all of the validation methods for storing into the arraylists
    because we have switched the storage method over to an actual sql server
    so I will rewrite all of the new sql data storage and validation code
    into the sqlconnection class file

--- DataSetProject/src/ToolBox.java
+++ DataSetProject/src/ToolBox.java
@@ -1,6 +1,8 @@
 //IMPORTS
 import java.util.ArrayList;
 import java.util.Random;
+
+import javax.swing.JFrame;
 /**
  * Date Created: 5/16/2022
  * Purpose: Toolbox full of our own methods and objects to be used in our main java program
@@ -42,102 +44,6 @@ public class ToolBox {
 		
 	}//end of captchaGen method
 	
-	
-	/*method will take in user input as parameter and the username array list checking for 
-	 * 1) username in the array list that matches user input
-	 * 2) returning the index position in the array
-	*/
-	public boolean usernameValidation(String username, ArrayList<String> arraylist) {
-		//creating the validation variable to return after array iteration
-		boolean verify = false;
-		
-		//Create the loop to iterate through the array list and check for matches to user input
-		for(int i = 0; i < arraylist.size(); i++) {
-			//checking through each iteration if the username or pass parameter equals the current value of the current index number in the arraylist
-			if(username.equals(arraylist.get(i))) {
-				verify = true;
-			}else {
-
-			}//end of if statement
-			
-		}//end of for loop
-		
-		//returning true or false if user input matches any strings in the array
-		return verify; 
-	}//end of username validation method
-	
-	public boolean passwordValidation(String password, ArrayList<String> arraylist) {
-		//creating the validation variable to return after array iteration
-		boolean verify = false;
-		
-		//Create the loop to iterate through the array list and check for matches to user input
-		for(int i = 0; i < arraylist.size(); i++) {
-			//checking through each iteration if the username or pass parameter equals the current value of the current index number in the arraylist
-			if(password.equals(arraylist.get(i))) {
-				verify = true;
-			}else {
-
-			}//end of if statement
-			
-		}//end of for loop
-		
-		//returning true or false if user input matches any strings in the array
-		return verify; 
-	}//end of username validation method
-	
-	
-	
-	//this method grabs the position of the username if applicable in the arraylist
-	public int userNamePos(String username, ArrayList<String> arraylist){		
-		int indexNumuser = -1;
-		
-		//Create the loop to iterate through the array list and check for matches to user input
-		for(int i = 0; i < arraylist.size(); i++) {
-			//checking through each iteration if the user or pass parameter value equals the current value of the current index number in the arraylist
-			if(username.equals(arraylist.get(i))) {
-				indexNumuser = i;
-			}else {
-			
-			}//end of if statement
-			
-		}//end of for loop
-		
-		return indexNumuser;
-			
-	}//end of usernamepos method
-	
-	
-	//this method grabs the position of the username if applicable in the arraylist
-		public int passNamePos(String password, ArrayList<String> arraylist){
-			//creating the index position holder variable
-			int indexNumpass = -2;
-			
-			//Create the loop to iterate through the array list and check for matches to user input
-			for(int i = 0; i < arraylist.size(); i++) {
-				//checking through each iteration if the user or pass parameter value equals the current value of the current index number in the arraylist
-				if(password.equals(arraylist.get(i))) {
-					indexNumpass = i;
-				}else {
-				
-				}//end of if statement
-				
-			}//end of for loop
-			
-			return indexNumpass;
-				
-		}//end of passnamepos method
-	
-	//checks that the position in the userarray matches the position in the passarray
-	public boolean positionValidation() {
-		return false;
-		
-	}//end of user and pass validation method
-	
-	public static void main(String[] args) {
-	
-	}//end of main method
-
-
 
 
 
